fce75bee2cc8a42ee697ec7fc586afeeb6665260
Slava Pleshkov <mail@slavapleshkov.com>
2019-02-09 19:00:06 +0200

    Heroku

--- config/db.php
+++ config/db.php
@@ -1,14 +1,23 @@
 <?php
+$host = $username = $password = $dbname = '';
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+if (isset($url["host"]) && isset($url["user"]) && isset($url["pass"]) && isset($url["path"])) {
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$dbname = substr($url["path"], 1);
+}
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2editorial',
-    'username' => 'yii2editorial',
-    'password' => 'yii2editorial',
-    'charset' => 'utf8',
+	'class' => 'yii\db\Connection',
+	'dsn' => 'postgres:host=' . $host . ';dbname=' . $dbname,
+	'username' => $username,
+	'password' => $password,
+	'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+	// Schema cache options (for production environment)
+	//'enableSchemaCache' => true,
+	//'schemaCacheDuration' => 60,
+	//'schemaCache' => 'cache',
 ];
